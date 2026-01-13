919718d157950442e614d281b535e8807b66df43
dev-casolari <82168802+dev-casolari@users.noreply.github.com>
2026-01-13 15:06:32 +0000

    Add Streamlit and base app

---
+++ app.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Movie Randomizer",
+    page_icon="🎬",
+)
+
+st.title("🎬 Movie Randomizer")
+st.write("Suggerimenti casuali per non perdere tempo a scegliere un film.")
